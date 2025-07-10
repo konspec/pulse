a3cee0df63e6d3d8a45fa3f8c167e8b74ccbee96
kon-fin <58323284+konspec@users.noreply.github.com>
2025-07-10 10:35:00 +0530

    feat(customers): add basic Customer model with admin and history tracking
    Introduces a new Customer model with code and name fields, including historical tracking via django-simple-history. Adds initial migration for the model and registers it in the Django admin with custom display and search fields.

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,1 +1,14 @@
-# Register your models here.
+from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
+
+from .models import Customer
+
+
+class CustomerAdmin(SimpleHistoryAdmin):
+    """Custom admin built for SimpleHistoryAdmin."""
+
+    list_display = ["code", "name"]
+    search_fields = ["code", "name"]
+
+
+admin.site.register(Customer, CustomerAdmin)
--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,1 +1,22 @@
-# Create your models here.
+from django.db import models
+from simple_history.models import HistoricalRecords
+
+
+class Customer(models.Model):
+    """Customer model. Synced with ERP."""
+
+    code = models.CharField(max_length=10, unique=True, null=False, blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False)
+
+    history = HistoricalRecords()
+
+    class Meta:
+        """Meta class for customer object."""
+
+        verbose_name = "Customer"
+        verbose_name_plural = "Customers"
+        ordering = ["code"]
+
+    def __str__(self) -> None:
+        """Return a string representation of the customer object."""
+        return f"({self.code}) {self.name}"
