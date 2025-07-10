9aee64a82924f08ce09d314a21abbab75dc7313d
kon-fin <58323284+konspec@users.noreply.github.com>
2025-07-10 16:42:03 +0530

    feat(customers): add admin for CustomerLedgerEntry with read-only permissions

--- apps/customers/admin.py
+++ apps/customers/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from simple_history.admin import SimpleHistoryAdmin
 
-from .models import Customer
+from .models import Customer, CustomerLedgerEntry
 
 
 class CustomerAdmin(SimpleHistoryAdmin):
@@ -23,4 +23,25 @@ class CustomerAdmin(SimpleHistoryAdmin):
         return False
 
 
+class CustomerLedgerEntryAdmin(SimpleHistoryAdmin):
+    """Custom admin built for Customer Ledger Entries."""
+
+    list_display = ["customer", "posting_date", "document_no", "amount", "remaining_amount"]
+
+    search_fields = ["document_no"]
+
+    def has_change_permission(self, request, obj=None):  # noqa: ARG002
+        """Check if user has edit permission for the model."""
+        return False
+
+    def has_delete_permission(self, request, obj=None):  # noqa: ARG002
+        """Check if user has delete permission for the model."""
+        return False
+
+    def has_add_permission(self, request, obj=None):  # noqa: ARG002
+        """Check if user has create permission for the model."""
+        return False
+
+
 admin.site.register(Customer, CustomerAdmin)
+admin.site.register(CustomerLedgerEntry, CustomerLedgerEntryAdmin)
