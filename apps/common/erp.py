d14dd5fe87ece4e7d10ba0e2f70a1744e7f97eec
kon-fin <58323284+konspec@users.noreply.github.com>
2025-07-10 13:54:10 +0530

    feat(common): add loguru logger to Odyn client initialization
    Configured loguru to log Odyn client activity to a file specified by settings.LOGS_DIR. The logger is now passed to the Odyn client for improved logging and debugging.

--- apps/common/erp.py
+++ apps/common/erp.py
@@ -1,9 +1,13 @@
 from django.conf import settings
+from loguru import logger
 
 from odyn import BasicAuthSession, Odyn
+
+logger.remove()
+logger.add(settings.LOGS_DIR / "odyn.log", level="DEBUG")
 
 
 def get_client() -> Odyn:
     """Get an authenticated instance of Odyn client."""
     session = BasicAuthSession(username=settings.ERP_USERNAME, password=settings.ERP_PASSWORD)
-    return Odyn(base_url=settings.ERP_BASE_URL, session=session)
+    return Odyn(base_url=settings.ERP_BASE_URL, session=session, logger=logger)
