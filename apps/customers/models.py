ec1a1979ab88dd4c01fb232291e94a4ef01aa294
kon-fin <58323284+konspec@users.noreply.github.com>
2025-07-10 16:41:09 +0530

    feat(customers): add CustomerLedgerEntry model to customers app

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -20,3 +20,38 @@ class Customer(models.Model):
     def __str__(self) -> None:
         """Return a string representation of the customer object."""
         return f"({self.code}) {self.name}"
+
+
+class CustomerLedgerEntry(models.Model):
+    """Customer ledger entry model. Synced with ERP."""
+
+    # FOREIGN KEY
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, null=False, blank=False)
+
+    # UNIQUE FIELD
+    entry_no = models.CharField(max_length=20, null=False, blank=False, unique=True)
+
+    # DOCUMENT DATA
+    posting_date = models.DateField(blank=False, null=False)
+    document_no = models.CharField(max_length=30, null=False, blank=False)
+    document_type = models.CharField(max_length=30, null=False, blank=True)
+
+    # BLOCK CUSTOMER LEDGER ENTRIES
+    block_reason = models.CharField(max_length=255, null=False, blank=True, default="")
+
+    # AMOUNT RELATED DATA
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+    remaining_amount = models.DecimalField(max_digits=20, decimal_places=2)
+
+    history = HistoricalRecords()
+
+    def __str__(self) -> str:
+        """String representation of the customer ledger entry."""
+        return f"({self.document_no}) - {self.amount}"
+
+    class Meta:
+        """Meta class for CustomerLedgerEntry model."""
+
+        verbose_name = "Customer Ledger Entry"
+        verbose_name_plural = "Customer Ledger Entries"
+        ordering = ["-entry_no"]
